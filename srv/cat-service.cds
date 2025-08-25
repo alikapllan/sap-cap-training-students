617e3be07a5a7b87e4635c620b96d79659ac1cc8
Ali Haydar Kaplan <alihaydar.kaplan@delaware.pro>
2025-08-25 14:44:48 +0000

    add a simple GET handler for Student entity to see the output in debug mode

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,7 +1,5 @@
 using my.student as schema from '../db/schema';
 
-service CatalogService {
+service CatalogService @(impl: './mysimplesrv') {
     @readonly entity Student as projection on schema.Student; // referring to the entity in schema.cds
-
-    function myFunctionName(msg:String) returns String;
 }
--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -1,7 +1,11 @@
-const mySrvDemo = (srv) => {
-  srv.on("myFunctionName", (req, res) => {
-    return "Hello World" + req.data.msg;
+const cds = require("@sap/cds");
+
+module.exports = (srv) => {
+  const { Student } = srv.entities; // use exposed entity
+
+  srv.on("READ", "Student", async (req) => {
+    const result = await cds.tx(req).run(SELECT.from(Student));
+    console.log(result);
+    return result;
   });
 };
-
-module.exports = mySrvDemo;
