bb9e1678183e7f9d297b0e996c2adb6c109393f1
Ali Haydar Kaplan <alihaydar.kaplan@delaware.pro>
2025-08-25 15:43:53 +0000

    docs: showcase alternative handler using cds.service.impl

--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -9,3 +9,22 @@ module.exports = (srv) => {
     return result;
   });
 };
+
+// Another way of writing
+module.exports = cds.service.impl(async function () {
+  const { Student } = this.entities;
+
+  this.on("READ", Student, async (req) => {
+    // const result = await SELECT.from(Student); --> also same result 
+    const result = await cds.tx(req).run(SELECT.from(Student));
+    /*
+    cds.tx(req) binds the query to the same connection/context as the request.
+    This ensures:
+      - If multiple DB operations happen, they are all committed/rolled back together.
+      - Authorizations / tenant info from req are respected.
+      - Works consistently in multi-tenant and production scenarios (HANA HDI, etc).
+    */
+    console.log(result);
+    return result;
+  });
+});
