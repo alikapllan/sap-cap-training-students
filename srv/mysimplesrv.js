74e903fe84ea814812332a5613e77a189ca0cb57
Ali Haydar Kaplan <alihaydar.kaplan@delaware.pro>
2025-08-25 16:23:15 +0000

    usage of WHERE statement in select

--- srv/mysimplesrv.js
+++ srv/mysimplesrv.js
@@ -15,8 +15,12 @@ module.exports = cds.service.impl(async function () {
   const { Student } = this.entities;
 
   this.on("READ", Student, async (req) => {
-    // const result = await SELECT.from(Student); --> also same result 
-    const result = await cds.tx(req).run(SELECT.from(Student));
+    // const result = await SELECT.from(Student); --> also same result
+
+    const result_where_hardcoded = await cds.tx(req).run(
+      SELECT.from(Student).where({ email: "alice.wonderland@example.com" }) // one way of where statement
+    );
+
     /*
     cds.tx(req) binds the query to the same connection/context as the request.
     This ensures:
@@ -24,6 +28,10 @@ module.exports = cds.service.impl(async function () {
       - Authorizations / tenant info from req are respected.
       - Works consistently in multi-tenant and production scenarios (HANA HDI, etc).
     */
+
+    // If caller passes ?$filter=email eq 'alice.wonderland@example.com' or /odata/v4/catalog/Student(email='alice.wonderland@example.com')
+    // CAP already translates it into req.query
+    const result = await cds.tx(req).run(req.query);
     console.log(result);
     return result;
   });
